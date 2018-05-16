09bbe7f072964b7dd7c8e662f718d98e12b7bd2b
CCDM05 <yonamg67@gmail.com>
2018-05-16 10:48:05 -0700

    Primer php

---
+++ php/primera.php
@@ -0,0 +1,25 @@
+<?php
+	$parametro= $_GET["parametro"];
+	$variable=0;
+	$otra="";
+	$puntoflotante=0.0;
+
+
+	for ($i=0; $i <10 ; $i++) { 
+		
+		print("textosin".$i."<br>");
+	}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hola mundo</title>
+</head>
+<body>
+
+	Hola bebe <?php print($parametro)   ?>
+	
+</body>
+</html>
